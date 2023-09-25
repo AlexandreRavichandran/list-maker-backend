5dd5cbc8b96200cad3f46a9d9159f7870f146bd2
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2023-09-25 20:40:28 +0200

    feat(appuser): Create appuser service

---
+++ service-parent/service-appuser/src/main/java/com/medialistmaker/appuser/AppUserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.medialistmaker.appuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppUserServiceApplication.class, args);
+    }
+}
