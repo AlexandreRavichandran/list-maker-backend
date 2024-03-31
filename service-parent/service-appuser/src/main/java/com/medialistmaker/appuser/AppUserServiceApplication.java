6c186834fba32f2e5b971f129f3e42f41a815e8e
Alexandre RAVICHANDRAN <alexandre.ravichandran@gmail.com>
2024-03-31 12:18:56 +0200

    fix(dependencies): Remove useless dependencies

--- service-parent/service-appuser/src/main/java/com/medialistmaker/appuser/AppUserServiceApplication.java
+++ service-parent/service-appuser/src/main/java/com/medialistmaker/appuser/AppUserServiceApplication.java
@@ -1,18 +1,13 @@
 package com.medialistmaker.appuser;
 
-import com.medialistmaker.appuser.domain.AppUser;
-import com.medialistmaker.appuser.repository.AppUserRepository;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@EnableFeignClients
 public class AppUserServiceApplication {
 
     public static void main(String[] args) {
@@ -31,16 +26,4 @@ public class AppUserServiceApplication {
         return modelMapper;
     }
 
-    //@Bean
-    CommandLineRunner run(AppUserRepository appUserRepository) {
-        return args -> {
-            AppUser appUser = AppUser
-                    .builder()
-                    .username("test")
-                    .password(this.passwordEncoder().encode("test"))
-                    .build();
-
-            appUserRepository.save(appUser);
-        };
-    }
 }
